850e9ca1be9094791e5d83d14bc8e4b128da2942
Harshbhati30 <bhatiharsh9818@gmail.com>
2025-08-10 18:31:09 +0530

    Loading Feature Added

--- Social-Media-App/src/components/PostList.jsx
+++ Social-Media-App/src/components/PostList.jsx
@@ -1,24 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect,useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+
+
 
 const PostList = () => {
   const { postList , addInitialPosts } = useContext(PostListData);
 
-  const handleGetPostsClick = ()=> {
-    fetch('https://dummyjson.com/posts')
+  const [fetching,setFetching] = useState(false);
+
+  useEffect(()=>{
+    setFetching(true);
+  fetch('https://dummyjson.com/posts')
       .then(res => res.json())
       .then(data => {
         addInitialPosts(data.posts);
+        setFetching(false);
       })
-
-  }
+},[]);
 
   return (
     <div className="row g-3 align-items-stretch">
-      {postList.length ===0  && <WelcomeMessage onGetPostsClick={handleGetPostsClick} /> }
-      {postList.map((post) => (
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length ===0  && <WelcomeMessage/> }
+      {!fetching && postList.map((post) => (
         <div key={post.id} className="col-md-6 d-flex">
           <div className="w-100 h-100">
             <Post post={post} />
--- Social-Media-App/src/components/WelcomeMessage.jsx
+++ Social-Media-App/src/components/WelcomeMessage.jsx
@@ -2,13 +2,6 @@ const WelcomeMessage = ({ onGetPostsClick }) => {
   return (
     <div className="welcome-message">
       <h1>There are No Posts.</h1>
-      <button
-        type="button"
-        className="btn btn-success"
-        onClick={onGetPostsClick}
-      >
-        Get Posts
-      </button>
     </div>
   );
 };
